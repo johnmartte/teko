9ae0c20bda7e3fe9abd6c90fd6e0a47fb2b06151
axxe26 <angelycabreja@icloud.com>
2026-07-20 23:54:40 -0400

    fix: correccion de import de ThemeProviderProps desde next-themes

--- components/providers/theme-provider.tsx
+++ components/providers/theme-provider.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider} from "next-themes";
-import type { ThemeProviderProps } from "next-themes/dist/types";
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   React.useEffect(() => {
@@ -20,6 +19,6 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
       console.error = originalError;
     };
   }, []);
-  
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
